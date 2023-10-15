ae999d9111aef1458877146b4bc4b1b1d4d219ac
Denis Riabchuk <Naimas@DESKTOP-68Q4MHG>
2023-10-15 18:03:55 +0300

    done

---
+++ KeyboardTrainingApp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinForm1._2
+{
+    internal class KeyboardTrainingApp
+    {
+    }
+}
