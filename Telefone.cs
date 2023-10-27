713be6b0f08dd92e212a49707f39524e37214ca8
Naimas1 <114872107+Naimas1@users.noreply.github.com>
2023-10-27 18:39:23 +0300

    Update Telefone.cs

--- Telefone.cs
+++ Telefone.cs
@@ -1,12 +1,82 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using NumberGuessingGame;
 
-namespace FinForm1._2
+public class ContactViewModel : ViewModelBase
 {
-    internal class Telefone
+    private string fullName;
+    private string address;
+    private string phoneNumber;
+
+    public string FullName
     {
+        get => fullName;
+        set { Set(() => FullName, ref fullName, value); }
+    }
+
+    private void Set(Func<string> value1, ref string fullName, string value2)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string Address
+    {
+        get => address;
+        set { Set(() => Address, ref address, value); }
+    }
+
+    public string PhoneNumber
+    {
+        get => phoneNumber;
+        set { Set(() => PhoneNumber, ref phoneNumber, value); }
+    }
+
+    public RelayCommand AddContactCommand { get; private set; }
+    public RelayCommand ClearFieldsCommand { get; private set; }
+
+    public ContactViewModel()
+    {
+        AddContactCommand = new RelayCommand(AddContact);
+        ClearFieldsCommand = new RelayCommand(ClearFields);
+    }
+
+    private void AddContact()
+    {
+        // Додавання контакту до списку або бази даних
+      
+    }
+
+    private void ClearFields()
+    {
+        FullName = "";
+        Address = "";
+        PhoneNumber = "";
+    }
+}
+
+public partial class MainForm : Form
+{
+    private ContactViewModel viewModel;
+
+    public ContactViewModel DataContext { get; private set; }
+
+    public MainForm()
+    {
+        InitializeComponent();
+        viewModel = new ContactViewModel();
+        DataContext = viewModel;
+    }
+
+    private void InitializeComponent()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void AddContactButton_Click(object sender, EventArgs e)
+    {
+        viewModel.AddContactCommand.Execute(null);
+    }
+
+    private void ClearFieldsButton_Click(object sender, EventArgs e)
+    {
+        viewModel.ClearFieldsCommand.Execute(null);
     }
 }
